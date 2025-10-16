e6e4d5f340fee3575e8f030109bd22cf1b9259b3
ADMIN <anhnguyenntyct@gmail.com>
2025-10-16 18:55:17 +0700

    form Đăng nhập nhennn

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PTUD_QLTV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmDangnhap());
         }
     }
 }
