0565b6d1bedf00901db2db63b0ac52bceb7895f7
Đinh Thị Thơm <dinhthithom09072005@gmail.com>
2025-11-11 10:49:02 +0700

    Revert "Thomdinh frmMain,PM,PP,TL"

--- FrmDangnhap.cs
+++ FrmDangnhap.cs
@@ -15,10 +15,11 @@ namespace PTUD_QLTV
         private Dictionary<string, string> taiKhoan = new Dictionary<string, string>()
         {
             { "11221432@neulib.com", "123456" },
-            { "user1", "abc123" },
-            {"11235660@gmail.com","090705" },
+            { "user1@gmail.com", "abc123" },
+            {"11235630@gmail.com","070905" },
             {"11235576@gmail.com","220605" },
             {"11235538@gmail.com","230505" }
+
         };
         private Dictionary<string, string> ghiNhoTaiKhoan = new Dictionary<string, string>();
 
@@ -26,7 +27,6 @@ namespace PTUD_QLTV
         {
             InitializeComponent();
             // Khi form load, kiểm tra có ghi nhớ tài khoản trước đó không
-            
             if (Properties.Settings.Default.Username != "")
             {
                 txttendn.Text = Properties.Settings.Default.Username;
@@ -55,6 +55,14 @@ namespace PTUD_QLTV
             txttendn.TextChanged += txttendn_TextChanged;
         }
 
+
+        
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
         private void txttendn_TextChanged(object sender, EventArgs e)
         {
             string username = txttendn.Text.Trim();
@@ -62,7 +70,9 @@ namespace PTUD_QLTV
                 txtmk.Text = ghiNhoTaiKhoan[username];
             else
                 txtmk.Clear();
+
         }
+
         private void FrmDangnhap_Load(object sender, EventArgs e)
         {
             if (Properties.Settings.Default.RememberMe)
@@ -77,11 +87,13 @@ namespace PTUD_QLTV
                 txtmk.Text = "";                                        // >>> THÊM MỚI
                 chkgnmk.Checked = false;                                // >>> THÊM MỚI
             }
+
         }
 
         private void btnthoat_Click(object sender, EventArgs e)
         {
             Application.Exit();
+
         }
 
         private void btndn_Click(object sender, EventArgs e)
@@ -92,6 +104,8 @@ namespace PTUD_QLTV
             // Kiểm tra tên đăng nhập và mật khẩu có đúng không
             if (taiKhoan.ContainsKey(username) && taiKhoan[username] == password)
             {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
                 // Nếu chọn ghi nhớ mật khẩu
                 if (chkgnmk.Checked)
                 {
@@ -114,9 +128,6 @@ namespace PTUD_QLTV
                 }
 
                 Properties.Settings.Default.Save();
-               
-                this.DialogResult = DialogResult.OK;
-                this.Close();
             }
             else
             {
@@ -124,8 +135,16 @@ namespace PTUD_QLTV
                 txttendn.Clear();
                 txtmk.Clear();
                 txttendn.Focus();
-            }           
+            }
+
         }
-    }   
+
+        private void linklbllaymk_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmlogindemo f = new frmlogindemo();
+            f.Show();
+            this.Hide();
+
+        }
+    }
 }
-  
